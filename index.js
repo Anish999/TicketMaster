d0de3724becaa39c44a2d73674d74fd4c1444dd3
Anish Manandhar <anish.manandhar@selu.edu>
2019-05-06 18:15:02 -0500

    error fixed

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ const app = express();
 
 var TICKETS_COLLECTION = "tickets";
 
-
 app.use(bodyParser.json());
+
+app.use(express.json());
 
 var db;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb+srv://Anish999:Anish123@phase2-415-n2ctl.mongodb.net/test?retryWrites=true", function(err, client){
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb+srv://Anish999:Anish123@phase2-415-n2ctl.mongodb.net/test?retryWrites=true", {useNewUrlParser: true},function(err, client){
     if (err) {
         console.log(err);
         process.exit(1);
     }
+    else{
 
-    db = client.db();
-    console.log("Database connection ready");
+        db = client.db('test');
+        console.log("Database connection ready");
+    }
 
     // var server = app.listen(process.env.PORT || 8080, function(){
     //     var port = server.address().port;
